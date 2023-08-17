1e719fa9f6e3ab8534e2b03de1f6c88a8366751b
ioloolo <sondaehyeon01@gmail.com>
2023-08-17 16:52:16 +0900

    Add log

--- src/main/java/com/github/ioloolo/teacher_plan/domain/timetable/TimeTableController.java
+++ src/main/java/com/github/ioloolo/teacher_plan/domain/timetable/TimeTableController.java
@@ -1,17 +1,26 @@
 package com.github.ioloolo.teacher_plan.domain.timetable;
 
-import com.github.ioloolo.comcigan.ComciganBaseApi;
-import com.github.ioloolo.comcigan.teacher.ComciganTeacherApi;
-import com.github.ioloolo.teacher_plan.domain.timetable.context.GetTeacherListRequest;
-import com.github.ioloolo.teacher_plan.domain.timetable.context.GetTimeTableRequest;
-import com.github.ioloolo.teacher_plan.domain.timetable.context.SearchSchoolRequest;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.ioloolo.comcigan.ComciganBaseApi;
+import com.github.ioloolo.comcigan.data.School;
+import com.github.ioloolo.comcigan.teacher.ComciganTeacherApi;
+import com.github.ioloolo.comcigan.teacher.data.Teacher;
+import com.github.ioloolo.comcigan.teacher.timetable.DailyTimeTable;
+import com.github.ioloolo.teacher_plan.domain.timetable.context.GetTeacherListRequest;
+import com.github.ioloolo.teacher_plan.domain.timetable.context.GetTimeTableRequest;
+import com.github.ioloolo.teacher_plan.domain.timetable.context.SearchSchoolRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/timetable")
 @RequiredArgsConstructor
@@ -19,16 +28,31 @@ public final class TimeTableController {
 
     @PostMapping("/school")
     public ResponseEntity<?> searchSchool(@RequestBody SearchSchoolRequest request) {
-        return ResponseEntity.ok(ComciganBaseApi.searchSchool(request.getQuery()));
+        String query = request.getQuery();
+
+        List<School> body = ComciganBaseApi.searchSchool(query);
+
+        return ResponseEntity.ok(body);
     }
 
     @PostMapping("/teachers")
     public ResponseEntity<?> getTeachers(@RequestBody GetTeacherListRequest request) {
-        return ResponseEntity.ok(ComciganTeacherApi.getTeacherList(request.getSchool()));
+        int school = request.getSchool();
+
+        List<Teacher> teacherList = ComciganTeacherApi.getTeacherList(school);
+
+        return ResponseEntity.ok(teacherList);
     }
 
     @PostMapping("/timetable")
     public ResponseEntity<?> getTimetable(@RequestBody GetTimeTableRequest request) {
-        return ResponseEntity.ok(ComciganTeacherApi.getWeeklyTimeTable(request.getSchool(), request.getTeacher()));
+        int school = request.getSchool();
+        int teacher = request.getTeacher();
+
+        List<DailyTimeTable> weeklyTimeTable = ComciganTeacherApi.getWeeklyTimeTable(school, teacher);
+
+        log.info("[TimeTable] Request. ({} - {})", school, teacher);
+
+        return ResponseEntity.ok(weeklyTimeTable);
     }
 }
